b9ec2166540913d1c25966bcd15693d68808d8f4
jamieloertscher <jamie.loertscher@gmail.com>
2026-05-21 10:43:17 +0200

    info page

--- webapp/app.py
+++ webapp/app.py
@@ -9,3 +9,15 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+    
+# Neue Route
+@app.route('/info', methods=['GET'])
+def info():
+    return jsonify({
+        "status": "running",
+        "version": "1.0.0",
+        "description": "Flask-App fuer das Git-Merge-Training"
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
